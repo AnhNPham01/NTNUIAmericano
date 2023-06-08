d511fc446563a34e602ff281a3c8204c549b9f4f
Anh <qapham@stud.ntnu.no>
2023-06-08 15:49:26 +0200

    Format rounds and style

---
+++ src/components/Container.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * A React functional component that ensures content within it is centered, has the correct
+ * background color, with and margin.
+ *
+ * @param props The content to be adjusted.
+ *
+ * @returns A React funcional component for ensuring content uniformity on different pages.
+ */
+const Container = (props: any) => {
+    return (
+        <div className="flex justify-center bg-white">
+            <div className="w-full font-sans m-5">{props.children}</div>
+        </div>
+    );
+};
+
+export default Container;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // pages/index.tsx
 import { GetServerSideProps, GetServerSidePropsResult } from "next";
+import Container from "@/components/Container";
 import Link from "next/link";
 
 interface Item {
@@ -11,6 +12,10 @@ interface Item {
 interface HomeProps {
     data: Item[] | null;
     error: string | null;
+}
+
+interface Match {
+    [index: number]: string[];
 }
 
 const sendData = async (
@@ -78,6 +83,32 @@ function generateRounds(data: any[]): any[][][] {
     return rounds;
 }
 
+type Matchup = string[][][];
+
+function formatMatchups(matchups: Matchup): string[][][] {
+    let rounds: string[][][] = [];
+
+    matchups.forEach((round, index) => {
+        let roundText: string[][] = [];
+        roundText.push([`Round ${index + 1}`]);
+
+        for (let i = 0; i < round.length; i += 2) {
+            if (
+                round[i].length > 0 &&
+                round[i + 1] &&
+                round[i + 1].length > 0
+            ) {
+                roundText.push([`${round[i][0]}`, `${round[i + 1][0]}`]);
+                roundText.push([`${round[i][1]}`, `${round[i + 1][1]}`]);
+            }
+        }
+
+        rounds.push(roundText);
+    });
+
+    return rounds;
+}
+
 export const getServerSideProps: GetServerSideProps<
     HomeProps
 > = async (): Promise<GetServerSidePropsResult<HomeProps>> => {
@@ -100,40 +131,75 @@ const Home: React.FC<HomeProps> = ({ data, error }) => {
     if (!data || !Array.isArray(data)) {
         return <div>Loading...</div>;
     }
-    return (
-        <div className="h-screen">
-            <p className="text-4xl">Americano</p>
-            <table>
-                <thead>
-                    <tr>
-                        <th>MedlemsID</th>
-                        <th>Fornavn</th>
-                        <th>Etternavn</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item) => (
-                        <tr
-                            key={`${item.medlemsid}-${item.fornavn}-${item.etternavn}`}
-                        >
-                            <td>{item.medlemsid}</td>
-                            <td>{item.fornavn}</td>
-                            <td>{item.etternavn}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
 
-            <div className="mt-2">
-                <p className="font-bold">Spillere</p>
-                {data.map((item) => (
-                    <div>
-                        {item.fornavn} {item.etternavn[0]}.
-                    </div>
-                ))}
+    const rounds = formatMatchups(generateRounds(data));
+    return (
+        <Container>
+            <div className="h-screen">
+                <p className="m-5 flex justify-center text-4xl">Americano</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>MedlemsID</th>
+                            <th>Fornavn</th>
+                            <th>Etternavn</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item) => (
+                            <tr
+                                key={`${item.medlemsid}-${item.fornavn}-${item.etternavn}`}
+                            >
+                                <td>{item.medlemsid}</td>
+                                <td>{item.fornavn}</td>
+                                <td>{item.etternavn}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <div className="mt-5">
+                    <p className="font-bold">Spillere</p>
+                    {data.map((item) => (
+                        <div key={`${item.fornavn}-${item.etternavn}`}>
+                            {item.fornavn} {item.etternavn[0]}.
+                        </div>
+                    ))}
+                </div>
+                <div className="text-center p-5">
+                    {rounds.map((round, index) => (
+                        <div key={index}>
+                            <div className="text-slate-700 font-bold mb-2">
+                                {round[0][0]}
+                            </div>
+                            <div className="bg-gray-100 rounded-md mb-4 max-w-md md:max-w-lg">
+                                <div className="flex items-center justify-between p-5">
+                                    <div className="flex flex-col">
+                                        {round
+                                            .slice(1)
+                                            .map((match, matchIndex) => (
+                                                <div key={matchIndex}>
+                                                    {match[0]}
+                                                </div>
+                                            ))}
+                                    </div>
+                                    {round.length > 1 && <div>vs</div>}
+                                    <div className="flex flex-col">
+                                        {round
+                                            .slice(1)
+                                            .map((match, matchIndex) => (
+                                                <div key={matchIndex}>
+                                                    {match[1]}
+                                                </div>
+                                            ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div>{generateRounds(data)}</div>
-        </div>
+        </Container>
     );
 };
 
